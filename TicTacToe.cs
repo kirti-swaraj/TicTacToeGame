e109768f881aeb41f6e47a13ca064fffb7681f6f
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-11 00:18:08 +0530

    [kirti] Add . Display the board

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -41,5 +41,17 @@ using System.Collections.Generic;
             string userLetter = Console.ReadLine();
             return char.ToUpper(userLetter[0]);
         }
+        /// <summary>
+        /// Shows the board.
+        /// </summary>
+        /// <param name="board">The board.</param>
+        public static void showBoard(char[] board)
+        {
+            Console.WriteLine("\n " + board[1] + "|" + board[2] + "|" + board[3]);
+            Console.WriteLine("--------");
+            Console.WriteLine(" " + board[4] + "|" + board[5] + "|" + board[6]);
+            Console.WriteLine("--------");
+            Console.WriteLine(" " + board[7] + "|" + board[8] + "|" + board[9]);
+        }
     }
 }
